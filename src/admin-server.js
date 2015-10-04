5bf510d54d985ddcd0384101b043d99d23b6a41f
Nick Sellen <git@nicksellen.co.uk>
2015-10-04 15:26:01 +0200

    return flat array instead of nested objects for connection status

--- src/admin-server.js
+++ src/admin-server.js
@@ -5,19 +5,16 @@ module.exports = function(io) {
   var app = express();
 
   app.get('/', function(req, res){
-    var users = {};
-    io.sockets.sockets.forEach(function(socket){
-      var data = socket.data;
-      var userId = data.userId || '__nouser__';
-      var sessionId = data.sessionId || '__nosession__';
-      if (!users[userId]) users[userId] = {};
-      if (!users[userId][sessionId]) users[userId][sessionId] = [];
-      users[userId][sessionId].push({
-        clientId: socket.id,
+    var connections = io.sockets.sockets.map(function(socket){
+      var data = socket.data || {};
+      return {
+        connectionId: socket.id,
+        sessionId: data.sessionId,
+        userId: data.userId,
         handshake: socket.handshake
-      });
+      };
     });
-    res.send({ users: users });
+    res.send({ connections: connections });
   });
 
   return app;
